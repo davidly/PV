32ef7d1f67efd6e306230e449f9e226c58b53748
David Lee <davidly_lee@hotmail.com>
2021-06-17 21:44:15 -0700

    undo environment change

--- m.bat
+++ m.bat
@@ -5,8 +5,6 @@ del pv.exe
 del pv.pdb
 @echo on
 
-call VsDevCmd.bat
-
 REM to build without LibRaw:
 REM rc pv.rc
 REM cl /nologo pv.cxx /MT /Ox /Qpar /O2 /Oi /Ob2 /EHac /Zi /Gy /DNDEBUG /D_AMD64_ /link pv.res /OPT:REF /subsystem:windows
